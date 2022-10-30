2cd8021da12093750655f7dd9cbe8a57aa139636
Hieu Le <hieule411@gmail.com>
2022-10-30 02:59:32 -0500

    updated index.js

--- server/index.js
+++ server/index.js
@@ -5,15 +5,15 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://hieule411:iluvmyhoney123@cluster0.nhpxgaj.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 3000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
